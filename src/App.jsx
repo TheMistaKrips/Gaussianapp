a23385152482de1b5bc4c8c9acc630ccba65f3bc
KripsaKrips <144921464+TheMistaKrips@users.noreply.github.com>
2024-03-05 01:07:02 +0200

    commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/cyK36zKuZzRa35Mw/scene.splinecode" />
+  );
+}
