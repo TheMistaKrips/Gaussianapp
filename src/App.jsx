f4de615322bf3f5f98838d2a789e4c3062d2ae8d
KripsaKrips <144921464+TheMistaKrips@users.noreply.github.com>
2024-03-05 01:17:43 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,6 @@ import Spline from '@splinetool/react-spline';
 
 export default function App() {
   return (
-    <Spline scene="https://prod.spline.design/cyK36zKuZzRa35Mw/scene.splinecode" />
+    <Spline scene="https://prod.spline.design/cyK36zKuZzRa35Mw/scene.splinecode"  />
   );
 }
